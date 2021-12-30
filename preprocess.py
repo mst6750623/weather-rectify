7baea671ef569feb39c329053fed508d3f33e2ea
harima <943941437@qq.com>
2021-12-30 19:49:39 +0800

    update preprocess of pearson correlation

--- preprocess.py
+++ preprocess.py
@@ -3,35 +3,60 @@ import os
 import yaml
 import argparse
 import numpy as np
+import pandas as pd
 from tqdm import tqdm
 from xarray import open_dataset
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--config', type=str, default='config')
 args = parser.parse_args()
-
 '''
 1229 todo:
 1.计算物理量之间的corrcoef(),先遍历拼成一个numpy array保存到一个文件（用例*物理量）
 2.对海拔不同的筛选出相关系数最高的
 '''
+
+
+def get_correlation(label_file):
+    lbl = np.load(label_file)
+    corr_ma = np.corrcoef(lbl.transpose())
+    return corr_ma
+    new_tensor = torch.from_numpy(corr_ma)
+    print(new_tensor.shape, new_tensor)
+
+
+def get_all_correlation(label):
+    correlation = []
+    for i in tqdm(range(40)):
+        filename = 'data/' + '{:02d}'.format(i + 1) + 'data.npy'
+        if i == 0:
+            correlation = get_correlation(filename)
+        else:
+            correlation += get_correlation(filename)
+    correlation = correlation / 40
+    print(correlation)
+    np.save('correlation.npy', correlation)
+
+
 def main():
     config_file = args.config + '.yaml'
     config = yaml.load(open(config_file, 'r'), Loader=yaml.FullLoader)
     train_dir = config['train_dir']
     print(train_dir)
-    no_record_tensor = torch.full((69, 73), -99999.).to('cuda')
+    #no_record_tensor = torch.full((69, 73), -99999.).to('cuda')
     result_list = []
-    for i in tqdm(range(1962)):
+    for i in tqdm(range(1901, 1962)):
         file_name = train_dir + 'example' + '{:0>5d}'.format(i + 1) + '/'
         for j in range(9):
             grid_file_name = file_name + 'grid_inputs_' + '{:0>2d}'.format(
                 j + 1) + '.nc'
+            if not os.path.isfile(grid_file_name):
+                continue
             ds = open_dataset(grid_file_name)
             vars_name = [name for name in ds]
             vars_values = [ds.data_vars[name].values for name in vars_name]
             #针对每个物理量
-            temp_list=[]
+            temp_list = []
             for values in vars_values:
                 '''new_tensor = torch.from_numpy(values).to('cuda')
                 if new_tensor.dim()==3:
@@ -46,18 +71,32 @@ def main():
                 result_list.append(temp_list.)
                 print(len(result_list))'''
                 if values.ndim == 3:
-                    values = np.transpose(values,(2,0,1))
+                    values = np.transpose(values, (2, 0, 1))
                     for num in range(values.shape[0]):
+                        #temp_list.append(values[num].flatten().tolist())
                         temp_list.append(values[num].flatten().tolist())
                 else:
+                    #temp_list.append(values.flatten().tolist())
                     temp_list.append(values.flatten().tolist())
-                
-                result_list.append(np.asarray(temp_list).transpose().tolist())
-                print(np.asarray(result_list).shape)
-            new_tensor = torch.Tensor(result_list).to('cuda')
-            print(new_tensor.shape)
-            return
+
+            result_list += np.asarray(temp_list).transpose().tolist()
+        if i % 50 == 0 and i != 0:
+            np_result = np.asarray(result_list)
+            name = '{:0>2d}'.format(int(i / 50)) + 'data'
+            name = os.path.join('data', name)
+            np.save(name + '.npy', np_result)
+            result_list = []
+            print(np_result.shape)
+        #print(np.asarray(result_list).shape)
+
+    #get_correlation('400data.npy')
     #print(len(result_list))
+    np_result = np.asarray(result_list)
+    name = '40data'
+    name = os.path.join('data', name)
+    np.save(name + '.npy', np_result)
+    result_list = []
+    print(np_result.shape)
 
 
 def print_single_point():
@@ -76,4 +115,5 @@ def print_single_point():
 
 
 if __name__ == "__main__":
-    main()
+    #main()
+    get_all_correlation('')
