ea2d2e7650ff2de755d8aed7acbe9a7cb9996b5a
harima <943941437@qq.com>
2022-01-03 23:24:45 +0800

    add encoder

--- net/encoder.py
+++ net/encoder.py
@@ -3,5 +3,9 @@ import torch.nn as nn
 
 
 class encoder(nn.Module):
-    def __init__(self, ):
+    def __init__(self, in_channels, mid_channels, out_channels):
         super(encoder, self).__init__()
+        self.layer1 = nn.Sequential(nn.Conv2d(in_channels, mid_channels))
+
+    def forward(self, input):
+        return self.layer1(input)
