05ccb23f3ccb2501214e0acd82484c0166596c6e
zzzzzx <1036201335@qq.com>
2022-01-10 21:35:57 +0800

    add combinatorial network as well as modify something

--- net/OrdinalRegression.py
+++ net/OrdinalRegression.py
@@ -4,13 +4,13 @@ from base_classifier import BaseClassifier
 
 
 class OrdinalRegressionNetwork(nn.Module):
-    def __init__(self, num_of_classes):
+    def __init__(self, in_channels, mid_channels, out_channels, num_of_classes):
         super(OrdinalRegressionNetwork, self).__init__()
         self.num_of_classes = num_of_classes
         self.classifiers = nn.ModuleList()
 
         for i in range(self.num_of_classes):
-            self.classifiers.append(BaseClassifier())
+            self.classifiers.append(BaseClassifier(in_channels, mid_channels, out_channels))
 
     def forward(self, x):
         ###每一类的概率####
