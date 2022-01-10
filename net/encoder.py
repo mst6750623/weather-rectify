00c399bf6e721a0d1bf786f1df503829a6c540c9
zzzzzx-1115 <73685937+zzzzzx-1115@users.noreply.github.com>
2022-01-10 11:28:16 +0800

    Add files via upload

--- net/encoder.py
+++ net/encoder.py
@@ -1,14 +1,42 @@
 import torch
 import torch.nn as nn
-
-
+from confidence import BasicConv2d
+#########zx 0110##########
 class encoder(nn.Module):
-    def __init__(self, in_channels, mid_channels, out_channels):
+    def __init__(self, in_channels, mid_channels, out_channels,
+                 mean = 0, std = 1e-1):
         super(encoder, self).__init__()
-        self.layer1 = nn.Sequential(nn.Conv2d(in_channels, mid_channels))
 
-    def forward(self, input):
-        return self.layer1(input)
+        ###args for Gaussian####
+        self.mean = mean
+        self.std = std
+
+        #self.layer1 = nn.Sequential(nn.Conv2d(in_channels, mid_channels))
+        self.layer1 = nn.Sequential(
+            BasicConv2d(in_channels, mid_channels, 1, bn = True),
+            BasicConv2d(mid_channels, mid_channels, 3, bn = True, padding = 1)
+        )
+        self.layer2 = nn.Sequential(
+            nn.MaxPool2d(2),
+            BasicConv2d(mid_channels, out_channels, 3, bn = True, padding = 1),
+            BasicConv2d(out_channels, out_channels, 3, bn = True, padding = 1)
+        )
+
+    def forward(self, x):
+        x = self.layer1(x)
+
+        if self.training:
+            x = x + torch.normal(
+                mean = torch.full(x.size(), fill_value = self.mean),
+                std = self.std
+            ).to(x.device)
+
+        x = self.layer2(x)
+        return x
+
+
+
+
 
 
 class MLP(nn.Module):
